1dedbce5ebbd5e7816ec270bf65f4b87d18d8495
natanlimaz <natanlimadev@gmail.com>
2024-04-18 14:16:14 -0300

    Order, Instant, ISO8601 - PART 2

---
+++ src/main/java/com/natan/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.natan.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.natan.course.entities.Order;
+import com.natan.course.repositories.OrderRepository;
+
+@Component
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
