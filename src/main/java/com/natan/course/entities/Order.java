0e0dfe85c913191dec85877a3f49a7818eb1941f
natanlimaz <natanlimadev@gmail.com>
2024-04-18 14:52:20 -0300

    OrderStatus enum

--- src/main/java/com/natan/course/entities/Order.java
+++ src/main/java/com/natan/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.natan.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +27,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id") //nome da FK no Banco
 	private User client;
@@ -33,9 +36,10 @@ public class Order implements Serializable{
 	public Order() {
 	}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 	
@@ -58,6 +62,17 @@ public class Order implements Serializable{
 	
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(this.orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
