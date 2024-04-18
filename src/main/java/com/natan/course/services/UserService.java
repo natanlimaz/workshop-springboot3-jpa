b42c1c70cd0c047a76c00818d25cd797ba23ab35
natanlimaz <natanlimadev@gmail.com>
2024-04-18 12:43:52 -0300

    Service Layer, component registration

---
+++ src/main/java/com/natan/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.natan.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.natan.course.entities.User;
+import com.natan.course.repositories.UserRepository;
+
+@Component
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
